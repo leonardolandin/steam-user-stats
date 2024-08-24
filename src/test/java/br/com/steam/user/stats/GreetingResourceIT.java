60c2eaf9c5ccab2ffcc1728762b856f4de7eb820
Leonardo Landin <leolandin123@gmail.com>
2024-08-24 00:50:06 -0300

    [INIT] - Iniciando projeto com dependencias

---
+++ src/test/java/br/com/steam/user/stats/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package br.com.steam.user.stats;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
